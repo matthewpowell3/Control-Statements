b036d7fd1de53b730c5fdb5dc1badb8b12232a74
matthewpowell3 <106132405+matthewpowell3@users.noreply.github.com>
2023-03-05 18:28:27 -0500

    1 file modified, 1 added, 1 deleted

---
+++ ifelse.js
@@ -0,0 +1,11 @@
+// Control Statements
+
+var x = 10;
+
+if (x < 1) {
+  document.write('The value is less than ten');
+} else if ((x = 10)) {
+  document.write('The value is equal to ten');
+} else {
+  document.write('The value is less than ten');
+}
