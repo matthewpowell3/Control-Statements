5b49ed9078e31223b1a6e961e175fc5c71ac031b
matthewpowell3 <106132405+matthewpowell3@users.noreply.github.com>
2023-03-05 18:31:14 -0500

    1 file modified

--- ifelse.js
+++ ifelse.js
@@ -2,10 +2,10 @@
 
 var x = 10;
 
-if (x < 1) {
-  document.write('The value is less than ten');
+if (x < 10) {
+  document.write('The value of x is less than ten');
 } else if ((x = 10)) {
-  document.write('The value is equal to ten');
+  document.write('The value of x is equal to ten');
 } else {
-  document.write('The value is less than ten');
+  document.write('The value of xis less than ten');
 }
