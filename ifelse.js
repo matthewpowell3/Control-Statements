dd57b5f2396f60e198971779143177aec4b84588
matthewpowell3 <106132405+matthewpowell3@users.noreply.github.com>
2023-03-05 18:46:39 -0500

    1 file modified

--- ifelse.js
+++ ifelse.js
@@ -1,11 +1,22 @@
 // Control Statements
 
-var x = 10;
+// If Statements
 
+/*
+var x = 10;
 if (x < 10) {
   document.write('The value of x is less than ten');
 } else if ((x = 10)) {
   document.write('The value of x is equal to ten');
 } else {
-  document.write('The value of xis less than ten');
+  document.write('The value of is greater than ten');
+} */
+
+var x = 6;
+//  % sign modulus the reminder of 6 divided by 2
+// two can go inti six evenly
+if (x % 2 == 0) {
+  document.write('Even Number');
+} else {
+  document.write('Odd Number');
 }
