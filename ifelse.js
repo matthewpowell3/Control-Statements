66bc27d35f9813a7cb0004c60a2d71bf5e113019
matthewpowell3 <106132405+matthewpowell3@users.noreply.github.com>
2023-03-05 21:19:01 -0500

    1 file modified

---
+++ ifelse.js
@@ -0,0 +1,67 @@
+// Control Statements
+
+// If Statements
+
+/*
+var x = 10;
+if (x < 10) {
+  document.write('The value of x is less than ten');
+} else if ((x = 10)) {
+  document.write('The value of x is equal to ten');
+} else {
+  document.write('The value of is greater than ten');
+} */
+
+/*
+var x = 6;
+//  % sign modulus the reminder of 6 divided by 2
+// two can go inti six evenly
+if (x % 2 == 0) {
+  document.write('Even Number');
+} else {
+  document.write('Odd Number');
+} */
+
+/*var y = 9;
+if (y > 0) {
+  document.write('Position Number');
+} else if (y < 0) {
+  document.write('Negative Number');
+} else {
+  document.write('Zero');
+} */
+
+/*
+var x = -11;
+
+if (x > 0) {
+  if (x % 2 == 0) {
+    document.write('Even');
+  } else {
+    document.write('Odd');
+  }
+} else if (x < 0) {
+  if (x % 2 == 0) {
+    document.write('Positive and even');
+  } else {
+    document.write('Negative and odd');
+  }
+} else {
+  document.write('Zero');
+} */
+
+var x = -30;
+
+if (x > 0) {
+  if (x > 0 && x % 2 == 0) {
+    document.write('Positive and even');
+  } else if (x > 0 && x % 2 != 0) {
+    document.write('Positive and odd');
+  }
+} else if (x < 0 && x % 2 == 0) {
+  document.write('Negative and even');
+} else if (x < 0 && x & (2 != 0)) {
+  document.write('Negative and odd');
+} else {
+  document.write('Zero');
+}
